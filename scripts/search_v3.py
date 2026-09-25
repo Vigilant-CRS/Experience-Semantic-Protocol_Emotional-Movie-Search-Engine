e1797476bdcaa40e7e2c06b8869b19ff749e9742
cipherdd <dulovic.damir@gmail.com>
2026-09-25 16:27:37 +0200

    Update scripts/search_v3.py corporate license header

--- scripts/search_v3.py
+++ scripts/search_v3.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2026 Damir Dulovic. All rights reserved.
+Copyright (c) 2026 Vigilant e.K. All rights reserved.
 Licensed under the Vigilant ESP Proprietary Software License (see LICENSE).
 Search V3 — query the mindread_v3 collection with hybrid dense+sparse fusion.
 
