a86dc3a78476c356bd7f8f8f86a3e10d0f131f29
cipherdd <dulovic.damir@gmail.com>
2026-09-25 16:26:30 +0200

    Update scripts/enrich_color_age.py corporate license header

--- scripts/enrich_color_age.py
+++ scripts/enrich_color_age.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2026 Damir Dulovic. All rights reserved.
+Copyright (c) 2026 Vigilant e.K. All rights reserved.
 Licensed under the Vigilant ESP Proprietary Software License (see LICENSE).
 Minimal-invasive enrichment: ergänze fehlende `color_palette` (weighted dict)
 und `protagonist_age` (single string) Felder in movies_dna_v3.jsonl, ohne
