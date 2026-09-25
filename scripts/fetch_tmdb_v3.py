a6f4824800975b717a04f2c67f304184bbd3bfa2
cipherdd <dulovic.damir@gmail.com>
2026-09-25 16:27:06 +0200

    Update scripts/fetch_tmdb_v3.py corporate license header

--- scripts/fetch_tmdb_v3.py
+++ scripts/fetch_tmdb_v3.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2026 Damir Dulovic. All rights reserved.
+Copyright (c) 2026 Vigilant e.K. All rights reserved.
 Licensed under the Vigilant ESP Proprietary Software License (see LICENSE).
 Fetch top-N films from TMDB by popularity and save to data/movies_tmdb_v3.json.
 
