deae2c7630e81c21ffbe0a1680459f75eec1b4bb
cipherdd <dulovic.damir@gmail.com>
2026-09-25 16:26:25 +0200

    Update scripts/audit_overviews.py corporate license header

--- scripts/audit_overviews.py
+++ scripts/audit_overviews.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2026 Damir Dulovic. All rights reserved.
+Copyright (c) 2026 Vigilant e.K. All rights reserved.
 Licensed under the Vigilant ESP Proprietary Software License (see LICENSE).
 Audit overview integrity by sampling against TMDB.
 
